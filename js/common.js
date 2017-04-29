4398963dda06b19db18e0e1dda19a30c3196494b
gunjubasich <gunjubasich@gmail.com>
2017-04-30 00:12:54 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -61,6 +61,51 @@ $(function() {
 	$('select').selectize({
 		create: true,
 	});
+	$('.reviews').owlCarousel({
+		loop: true,
+		autoplay: true,
+		items: 1,
+		smartSpeed: 700,
+		autoHeight: true,
+	});
+
+	$('.partners').owlCarousel({
+		loop: true,
+		autoplay: true,
+		items: 1,
+		dots: false,
+		smartSpeed: 700,
+		responsiveClass: true,
+		responsive: {
+			0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        },
+        1200: {
+        	items: 4
+        }
+	},
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else	{
+			$('.top').removeClass('active');
+		}
+	});
+
+	$('.top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 
 //E-mail Ajax Send
 	$("form.callback").submit(function() { //Change изменить селектор формы с помощью которого обрабатывается скрипт
@@ -80,4 +125,8 @@ $(function() {
 		return false;
 	});
 
+});
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
 });
